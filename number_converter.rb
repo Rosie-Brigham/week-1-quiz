af9cd5684252a6df25c94bfc99dcd987429f6079
Rosie Brigham <rbrigham@FC-289.local>
2014-03-06 18:07:29 +0000

    Quiz not quite complete

--- number_converter.rb
+++ number_converter.rb
@@ -1,3 +1,24 @@
 def convert_number(number)
-  # ???
+  if number > 5 
+  number * 3
+  else number <= 5  
+  	number-3
+  end
 end
+# if the argument passed is greater than 5, it returns 3 times the number,
+#   and if it is less than or equal to 5 it returns the number minus 3.
+
+array_h = ["youre","a","wizard","arry"]
+
+def caps(array)
+	
+	array.each do |word| 
+		puts word.upcase
+	end
+
+end
+
+
+
+# 5) Write a method that takes one argument (an array full of strings) 
+# and `puts` each word separately, but in uppercase.
